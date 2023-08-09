502c068ebd209639be9e436bd09d6c1d5cf2f43d
sarjilkhan <sarjilahmedkhan691@gmail.com>
2023-08-09 23:57:03 +0530

    Day-6

---
+++ Pointers/play.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //pointer to int is created and pointing at some garbage address
+    /* int *p = 0;
+
+    cout << *p<<endl */;
+ /*    int i =5;
+    
+    int *q = &i;
+    cout<< q <<endl;
+    cout<< *q <<endl;
+
+    int *p = 0 ;
+    p = &i;
+
+    cout<< p << endl;
+    cout<< *p <<endl; */
+
+    int num = 5;
+    int a = num;
+    cout<<"before "<<num<<endl;
+    a++;
+    cout<< "after "<<num<<endl;
+
+    int *p =&num;
+    cout<<"before "<<num<<endl;
+    (*p)++;
+    cout<< "after "<<num<<endl;
+
+    //copying a pointer
+    int *q = p;
+    cout<< p << " - "<<q <<endl;
+    cout<< *p << " - "<<*q <<endl;
+    
+    //important concept
+    int i = 3;
+    int *t = &i;
+    // cout<< (*t)++ <<endl;
+    *t = *t +1;
+    cout<< *t <<endl;
+    cout<< "before t "<< t <<endl;
+    t = t+1;
+    cout<< "after t "<< t <<endl;
+    return 0;
+}
